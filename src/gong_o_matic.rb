ec09257fda602400b1a9a25e0b08437a0acd6592
AlexBaranosky <alexander.baranosky@gmail.com>
2011-01-24 17:57:04 -0500

    refactored the multiplier to be an argument to the GongOMatic

--- src/gong_o_matic.rb
+++ src/gong_o_matic.rb
@@ -3,23 +3,24 @@ require File.dirname(__FILE__) + '/../src/console_printer'
 require File.dirname(__FILE__) + '/../src/audio_player'
 
 class GongOMatic
-  MULTIPLIER = 1.50
 
-  def run(file_name)
-    routine_parts = read_practice_routine(file_name)
+  def run(file_name, multiplier)
+    @multiplier = multiplier
+    @file_name = file_name
+    routine_parts = read_practice_routine()
     ConsolePrinter.new.print_schedule(routine_parts)
     AudioPlayer.new.play(routine_parts)
   end
 
   private
 
-  def read_practice_routine(file_name)
-    raise('practice routine file not found') unless File.exists?(file_name)
+  def read_practice_routine()
+    raise('practice routine file not found') unless File.exists?(@file_name)
 
-    File.open(file_name).inject([]) do |parts, line|
+    File.open(@file_name).inject([]) do |parts, line|
       part = PracticeRoutinePartParser.parse(line)
       if part
-        part.scale_duration_by(MULTIPLIER)
+        part.scale_duration_by(@multiplier)
         parts << part
       else
         parts
--- src/run_gong_o_matic.rb
+++ src/run_gong_o_matic.rb
@@ -3,4 +3,4 @@ require File.dirname(__FILE__) + '/../src/gong_o_matic'
 KUNGFU_STANCES_ROUTINE = File.dirname(__FILE__) + '/../resources/kungfu_stances_routine.txt'
 
 gong_o_matic = GongOMatic.new
-gong_o_matic.run(KUNGFU_STANCES_ROUTINE)
+gong_o_matic.run(KUNGFU_STANCES_ROUTINE, 1.01)
